e294290d02939349e9cc24076c5b14429a5d68fb
Ahmed Hamada Bakr <ahmedhamadabakr77@gmail.com>
2025-03-22 13:51:34 +0200

    add JSON parsing and new API endpoint for posts

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ const errorhandler = require("errorhandler");
 const app = express();
 const port = 8000;
 
-app.use(morgan("dev"));// log all requests to the console in dev mode about the request from the client
+app.use(morgan("dev")); // log all requests to the console in dev mode about the request from the client
 app.use(errorhandler());
-app.use(express.static("public"));// serve static files from the public directory
+app.use(express.static("public")); // serve static files from the public directory
+app.use(express.json()); // parse JSON data from the request body
 
 app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/api/posts", (req, res) => {
+  console.log(req.body);
   res.send("Hello World!");
 });
 
